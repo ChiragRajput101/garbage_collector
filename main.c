5e331d812c79faaffab44d570eeb1818300f0251
ChiragRajput101 <chiragrajput245@outlook.com>
2024-08-21 11:36:56 +0530

    fix: memory corruption due to alignment lesser than sizeof mem_chunk

--- main.c
+++ main.c
@@ -9,12 +9,12 @@
     2. handle fragmentation - maintain a list of currently allocated and free chunks - chunk metadata moves b/w these 2
     3. merge/colesce consectutive (wrt addr) free chunks to avoid fragmentation 
     4. should handle 0 size mem alloc req, that can be later passed to free(void *ptr)
-    5. explore alignment
 */ 
 
 void err(const char *message) {
     perror(message);
 }
+
 
 // embedded list
 typedef struct mem_chunk {
@@ -27,8 +27,13 @@ static mem_chunk sink; // 0 size sink for the free list
 // init the free_list (on Data Segment)
 static mem_chunk *free_list = &sink;
 
+
+static mem_chunk sink2;
 static mem_chunk *allocated_list;
 
+size_t align(size_t bytes) {
+    return ((bytes + sizeof(mem_chunk) - 1) & ~(sizeof(mem_chunk) - 1));
+}
 
 // ordered
 // insert: O(N), merge: O(1)
@@ -47,10 +52,8 @@ void insert_on_free_list(mem_chunk *ptr) {
     }
 
     if(found) {
-        // insert b/w t and t->next
-
         // coalescing w/ the free chunk ahead
-        if(t->next && (ptr + ptr->size == t->next)) {
+        if(t->next && (ptr + ptr->size + sizeof(mem_chunk) == t->next)) {
             ptr->size += t->next->size;
             ptr->next = t->next->next;
         } else {
@@ -58,7 +61,7 @@ void insert_on_free_list(mem_chunk *ptr) {
         }
 
         // coalescing w/ the free chunk before
-        if(t + t->size == ptr) {
+        if(t + t->size + sizeof(mem_chunk) == ptr) {
             t->size += ptr->size;
             t->next = ptr->next;
         } else {
@@ -84,9 +87,10 @@ size_t get_free_list(void) {
 
 // calls sbrk
 mem_chunk *incr_brk(size_t bytes) {
-    bytes = (bytes + sizeof(size_t) - 1) & ~(sizeof(size_t) - 1); // alignment to the nearest (upper) multiple of 8
+    bytes = align(bytes);
     bytes += sizeof(mem_chunk);
 
+    printf("sbrk: %zu\n", bytes);
     void *ret;
     if((ret = sbrk(bytes)) == (void *)-1) {
         return NULL;
@@ -100,53 +104,60 @@ mem_chunk *incr_brk(size_t bytes) {
 } 
 
 void *memalloc(size_t size) {
+    size = align(size);
+    printf("looking for %zu\n", size);
+
     mem_chunk *pt = free_list, *t = free_list->next;
     bool found = 0;
 
-    mem_chunk *mem;
+    mem_chunk *mem = NULL;
 
     while(t) {
         if(t->size >= size) {
+            printf("found: %p with size %zu\n", t, t->size);
             mem = t;
             if(t->size == size) {
                 pt->next = t->next;
-                t->next = allocated_list;
-                allocated_list = t;
+                // mem->next = allocated_list;
+                // allocated_list = mem;
                 
             } else {
                 // break the big chunk
-                mem->next = allocated_list;
-                allocated_list = mem;
+                // mem->next = allocated_list;
+                // allocated_list = mem;
 
-                t->size -= size;
-                t  = (mem_chunk *)((char *)t + size);
-                pt->next = t;
+                mem_chunk *new_chunk = (mem_chunk *)((char *)t + sizeof(mem_chunk) + size); // t -> |metadata|available mem|
+                new_chunk->size = t->size - size;
+                pt->next = new_chunk;
+                new_chunk->next = t->next;
             }
-            found = 1;
-            break;
+
+            return (char *)mem + sizeof(mem_chunk);
         }
 
         pt = t;
         t = t->next;
     }
 
-    if(!found) {
-        mem_chunk *p = incr_brk(size);
-        if(p == NULL) {
-            err("Unable to allocate more memory");
-            return NULL;
-        }
-        return (char *)p + sizeof(mem_chunk);
+    mem_chunk *p = incr_brk(size);
+    if(p == NULL) {
+        err("Unable to allocate more memory");
+        return NULL;
     }
-
-    return (char *)mem + sizeof(mem_chunk);
+    return (char *)p + sizeof(mem_chunk);
+    
 }
 
 int main() {
-    void *p1 = memalloc(8);
+    // void *p = memalloc(10);
+    // void *p1 = memalloc(30);
+    // void *p2 = memalloc(35);
+    void *p3 = memalloc(50);
+    void *p4 = memalloc(70);
     printf("free list contains: %zu chunks\n", get_free_list());
-    printf("usable at: %p\n", p1);
-    
+    void *p = memalloc(32);
+    printf("\n\n");
+    printf("free list contains: %zu chunks\n", get_free_list());
 
     return 0;
 }
